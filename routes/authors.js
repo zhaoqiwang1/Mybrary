9f0636e7f0511767a9f470a1830312393a6036d8
zhaoqiwang1 <wzqbewater@gmail.com>
2025-04-29 10:29:55 +0800

    let authors.js be able to access author.js model

--- routes/authors.js
+++ routes/authors.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const Author = require('../models/author') // give us the access to the author.js model. 
 
 // All Authors Route
 router.get('/', (req, res) => {
@@ -9,7 +10,7 @@ router.get('/', (req, res) => {
 
 // New Author Route: this route is just for displaying the form, the route below is actually creating the author.
 router.get('/new', (req, res) => {
-    res.render('authors/new')
+    res.render('authors/new', { author: new Author ()})
 })
 //Create Author Route: this one creates new authors.
 router.post('/', (req, res) => {
